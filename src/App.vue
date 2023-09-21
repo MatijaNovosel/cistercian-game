720515514b153bc85af6d02c67e5a91e8cc04f69
Matija Novosel <mnovosel5@gmail.com>
2023-09-21 10:27:25 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script setup lang="ts">
+//
+</script>
+
+<style scoped>
+.bottom-right {
+  position: fixed;
+  bottom: 25px;
+  right: 25px;
+  display: flex;
+  gap: 10px;
+}
+
+.result {
+  position: absolute;
+  font-size: 64px;
+  font-weight: bold;
+  color: white;
+  top: 25px;
+  left: 25px;
+}
+</style>
