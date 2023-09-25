9e40695e70250e8c0ee4260c836c6b100d45e0cc
Matija Novosel <mnovosel5@gmail.com>
2023-09-25 15:09:11 +0200

    Added pinia, toast wip

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
+  <toasts />
   <main>
     <h1>
       {{ n }}
     </h1>
     <characters width="300" height="300" :number="n" />
-    <button @click="change">Change</button>
+    <div class="inputs">
+      <input v-model="guessNumber" />
+      <button @click="guess">Guess</button>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
+import { randInt } from "matija-utils";
 import { ref } from "vue";
 import characters from "./components/characters.vue";
+import toasts from "./components/toasts.vue";
 
-const n = ref(9433);
+const n = ref(randInt(1, 9999));
+const guessNumber = ref<string | null | undefined>("");
 
-const change = () => {
-  n.value++;
+const generateNumber = () => {
+  n.value = randInt(1, 9999);
+};
+
+const guess = () => {
+  if (guessNumber.value === "" || !guessNumber.value) {
+    return;
+  }
+  console.log("bruh");
 };
 </script>
 
 <style scoped>
 main {
-  padding: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100%;
   flex-direction: column;
   gap: 40px;
+}
+
+.inputs {
+  display: flex;
+  gap: 20px;
 }
 </style>
---
+++ src/components/toasts.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="toast-ctr">
+    <div class="toast" v-for="{ text } in toasts">
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const toasts = ref([
+  {
+    text: "toast 1"
+  },
+  {
+    text: "toast 2"
+  }
+]);
+</script>
+
+<style scoped>
+.toast-ctr {
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  top: 5px;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+}
+
+.toast {
+  background-color: rgb(49, 124, 49);
+  padding: 8px;
+  border-radius: 4px;
+  color: white;
+  font-size: 14px;
+}
+</style>
