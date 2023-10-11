e7b60b4f39dadabb7f5c491b5eadb07dee2ff3f2
Matija Novosel <mnovosel5@gmail.com>
2023-10-11 15:46:13 +0200

    SVG shape changes

--- src/components/characters.vue
+++ src/components/characters.vue
@@ -12,129 +12,130 @@
       v-show="layout.uld2 === 1"
       width="10.6968"
       height="262.271"
-      rx="8"
-      transform="matrix(-.51543 1.04876-.531185-.261061 152.97663 67.091148)"
+      rx="0"
+      ry="0"
+      transform="matrix(-.473899 1.068169-.541015-.240026 147.087223 63.574405)"
       stroke-width="0"
-      style="fill: white"
+      fill="#fff"
     />
     <!-- Up right diagonal 2 -->
     <rect
       v-show="layout.urd2 === 1"
       width="10.6968"
       height="262.271"
-      rx="8"
-      transform="matrix(.493859 1.05908-.536416 0.250135 290.335686 1.377558)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(.493859 1.05908-.536416 0.250135 294.851058 0.622494)"
+      fill="#fff"
     />
     <!-- Down right diagonal 2 -->
     <rect
       v-show="layout.drd2 === 1"
       width="10.6968"
       height="262.271"
-      rx="8"
-      transform="matrix(.493859-1.05908 0.536416 0.250135 144.135783 228.14682)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(.510958-1.050937 0.563146 0.273797 146.82791 226.805899)"
+      fill="#fff"
     />
     <!-- Down left diagonal 2 -->
     <rect
       v-show="layout.dld2 === 1"
       width="10.6968"
       height="262.271"
-      rx="8"
-      transform="matrix(.493859 1.05908-.536416 0.250135 145.551 216.818016)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(.503203 1.054672-.549149 0.262008 144.151245 218.749794)"
+      fill="#fff"
     />
     <!-- Middle (m) -->
     <rect
       width="10.6968"
       height="262.271"
-      rx="8"
+      rx="0"
+      ry="0"
       transform="matrix(1.16857 0 0 1.14385 143.599 0)"
-      stroke-width="0"
-      style="fill: white"
+      fill="#fff"
     />
     <!-- Down right diagonal (drd) -->
     <rect
       v-show="layout.drd1 === 1"
       width="10.6968"
       height="262.271"
-      rx="8"
-      transform="matrix(.493859 1.05908-.536416 0.250135 287.579 218.012)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(.537777 1.037469-.535078 0.277359 294.381278 212.771694)"
+      fill="#fff"
     />
     <!-- Down left diagonal (dld) -->
     <rect
       v-show="layout.dld1 === 1"
       width="10.6968"
       height="262.271"
-      rx="8"
-      transform="matrix(.493859-1.05908 0.536416 0.250135 0.92329 228.397)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(.52561-1.043686 0.537753 0.270817 0.00549 225.310325)"
+      fill="#fff"
     />
     <!-- Up left diagonal (uld) -->
     <rect
       v-show="layout.uld1 === 1"
       width="10.6968"
       height="262.271"
-      rx="8"
-      transform="matrix(.493859 1.05908-.536416 0.250135 145.551 0)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(.500306 1.05605-.534882 0.2534 140.289412 2.965616)"
+      fill="#fff"
     />
     <!-- Up right diagonal (urd) -->
     <rect
       v-show="layout.urd1 === 1"
       width="10.6968"
       height="262.271"
-      rx="8"
-      transform="matrix(-.51543 1.04876-.531185-.261061 292.291 69.0912)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(-.470812 1.069533-.541706-.238463 300.133768 70.294241)"
+      fill="#fff"
     />
     <!-- Up top right (utr) -->
     <rect
       v-show="layout.utr === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
+      rx="0"
+      ry="0"
       transform="matrix(0 1.16857-.563865 0 300 0)"
-      stroke-width="0"
-      style="fill: white"
+      fill="#fff"
     />
     <!-- Up top left (utl) -->
     <rect
       v-show="layout.utl === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
-      transform="matrix(0 1.16857-.563865 0 143.599 0)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(0 1.16857-.562295 0 143.599 0)"
+      fill="#fff"
     />
     <!-- Up left (ul) -->
     <rect
       v-show="layout.ul === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
-      transform="matrix(-1.16857 0 0-.280852 12.099125 81.937213)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(-1.16857 0 0-.318625 12.500063 81.369916)"
+      fill="#fff"
     />
     <!-- Up right (ur) -->
     <rect
       v-show="layout.ur === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
-      transform="matrix(-1.16857 0 0-.280852 300.099 81.937213)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(-1.16857 0 0-.320846 299.999844 81.9372)"
+      fill="#fff"
     />
     <!-- Down left (dl) -->
     <rect
@@ -142,79 +143,79 @@
       width="10.6968"
       height="255.38"
       rx="8"
+      ry="8"
       transform="matrix(-1.16857 0 0-.319133 12.5 294)"
-      stroke-width="0"
-      style="fill: white"
+      fill="#fff"
     />
     <!-- Down right (dr) -->
     <rect
       v-show="layout.dr === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
-      transform="matrix(-1.16857 0 0-.319133 300.099001 294.943964)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(-1.16857 0 0-.342627 300.099 300)"
+      fill="#fff"
     />
     <!-- Down top right (dtr) -->
     <rect
       v-show="layout.dtr === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
+      rx="0"
+      ry="0"
       transform="matrix(0-1.16857 0.563865 0 156.099 225)"
-      stroke-width="0"
-      style="fill: white"
+      fill="#fff"
     />
     <!-- Up bottom right (ubr) -->
     <rect
       v-show="layout.ubr === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
+      rx="0"
+      ry="0"
       transform="matrix(0-1.16857 0.563865 0 156 82.25)"
-      stroke-width="0"
-      style="fill: white"
+      fill="#fff"
     />
     <!-- Up bottom left (ubl) -->
     <rect
       v-show="layout.ubl === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
-      transform="matrix(0-1.16857 0.563865 0 5.84864 82.25)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(0-1.16857 0.562295 0 0.000103 81.9372)"
+      fill="#fff"
     />
     <!-- Down top left (dtl) -->
     <rect
       v-show="layout.dtl === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
-      transform="matrix(0-1.16857 0.563865 0 0.598639 225)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(0-1.16857 0.562295 0 0 224.999774)"
+      fill="#fff"
     />
     <!-- Down bottom left (dbl) -->
     <rect
       v-show="layout.dbl === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
-      transform="matrix(0-1.16857 0.563865 0-.092051 300)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(0-1.16857 0.562295 0 0.000103 300.00018)"
+      fill="#fff"
     />
     <!-- Down bottom right (dbr) -->
     <rect
       v-show="layout.dbr === 1"
       width="10.6968"
       height="255.38"
-      rx="8"
-      transform="matrix(0 1.16857-.563865 0 300.099 287.5)"
-      stroke-width="0"
-      style="fill: white"
+      rx="0"
+      ry="0"
+      transform="matrix(0 1.16857-.563865 0 300.099 287.498723)"
+      fill="#fff"
     />
   </svg>
 </template>
