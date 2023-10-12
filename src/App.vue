66dce432b23af09101b685e77c8b0a971ce50746
Matija Novosel <mnovosel5@gmail.com>
2023-10-12 12:34:59 +0200

    Ref replaced with reactive

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,38 @@
   <toasts />
   <div class="top-right">
     <div class="attempts" :style="attemptsCtrStyle">
-      {{ numberOfAttempts }} / {{ MAX_NUMBER_OF_ATTEMPTS }}
+      {{ state.numberOfAttempts }} / {{ MAX_NUMBER_OF_ATTEMPTS }}
     </div>
     <div @click="showHelp" class="help">?</div>
   </div>
   <main>
-    <div class="finish-ctr" v-if="finished">
+    <div class="finish-ctr" v-if="state.finished">
       <img src="/cat.gif" />
       <h1>You did it! 🎉🎉🎉</h1>
     </div>
     <div v-else>
       <div class="characters-ctr">
-        <characters class="characters" width="70%" height="70%" :number="n" />
-        <spinner v-show="loading" class="spinner" />
+        <characters
+          class="characters"
+          width="70%"
+          height="70%"
+          :number="state.n"
+        />
+        <spinner v-show="state.loading" class="spinner" />
       </div>
       <div class="inputs">
         <div class="numbers">
-          <input type="number" class="number" v-model="guessDigit1" />
-          <input type="number" class="number" v-model="guessDigit2" />
-          <input type="number" class="number" v-model="guessDigit3" />
-          <input type="number" class="number" v-model="guessDigit4" />
+          <input type="number" class="number" v-model="state.guessDigit1" />
+          <input type="number" class="number" v-model="state.guessDigit2" />
+          <input type="number" class="number" v-model="state.guessDigit3" />
+          <input type="number" class="number" v-model="state.guessDigit4" />
         </div>
         <btn @click="guess">Guess</btn>
       </div>
     </div>
   </main>
   <teleport to="body">
-    <modal :show="showModal" @close="showModal = false">
+    <modal :show="state.showModal" @close="state.showModal = false">
       <template #header>
         <h3>Explanation</h3>
       </template>
@@ -43,7 +48,7 @@
 import { onKeyStroke } from "@vueuse/core";
 import confetti from "canvas-confetti";
 import { randInt } from "matija-utils";
-import { computed, ref } from "vue";
+import { computed, reactive } from "vue";
 import btn from "./components/btn.vue";
 import characters from "./components/characters.vue";
 import modal from "./components/modal.vue";
@@ -52,72 +57,72 @@ import toasts from "./components/toasts.vue";
 import { useToastStore } from "./stores";
 
 const MAX_NUMBER_OF_ATTEMPTS = 5;
-
 const toastStore = useToastStore();
 
-const n = ref(randInt(1, 9999));
-const numberOfAttempts = ref(0);
-const loading = ref(false);
-const showModal = ref(false);
-const finished = ref(false);
-
-const guessDigit1 = ref<string | null | undefined>("0");
-const guessDigit2 = ref<string | null | undefined>("0");
-const guessDigit3 = ref<string | null | undefined>("0");
-const guessDigit4 = ref<string | null | undefined>("0");
+const state = reactive({
+  n: randInt(1, 9999),
+  numberOfAttempts: 0,
+  loading: false,
+  showModal: false,
+  finished: false,
+  guessDigit1: "0",
+  guessDigit2: "0",
+  guessDigit3: "0",
+  guessDigit4: "0"
+});
 
 const attemptsCtrStyle = computed(() => ({
-  backgroundColor: finished.value ? "#4BB543" : "#FA7966"
+  backgroundColor: state.finished ? "#4BB543" : "#FA7966"
 }));
 
 const generateNumber = () => {
-  n.value = randInt(1, 9999);
+  state.n = randInt(1, 9999);
 };
 
 const reset = () => {
-  guessDigit1.value = "0";
-  guessDigit2.value = "0";
-  guessDigit3.value = "0";
-  guessDigit4.value = "0";
+  state.guessDigit1 = "0";
+  state.guessDigit2 = "0";
+  state.guessDigit3 = "0";
+  state.guessDigit4 = "0";
 };
 
 const showHelp = () => {
-  showModal.value = true;
+  state.showModal = true;
 };
 
 const guess = () => {
   if (
-    !guessDigit1.value ||
-    !guessDigit2.value ||
-    !guessDigit3.value ||
-    !guessDigit4.value
+    !state.guessDigit1 ||
+    !state.guessDigit2 ||
+    !state.guessDigit3 ||
+    !state.guessDigit4
   ) {
     return;
   }
-  const combined = `${guessDigit1.value}${guessDigit2.value}${guessDigit3.value}${guessDigit4.value}`;
+  const combined = `${state.guessDigit1}${state.guessDigit2}${state.guessDigit3}${state.guessDigit4}`;
   const parsed = parseInt(combined);
   if (combined.length > 4 || Number.isNaN(parsed)) {
     return;
   }
-  if (parsed === n.value) {
-    numberOfAttempts.value++;
+  if (parsed === state.n) {
+    state.numberOfAttempts++;
     toastStore.add("Good job!", "#4BB543");
-    if (numberOfAttempts.value === MAX_NUMBER_OF_ATTEMPTS) {
+    if (state.numberOfAttempts === MAX_NUMBER_OF_ATTEMPTS) {
       confetti({
         particleCount: 100,
         spread: 70,
         origin: { y: 0.6 }
       });
       toastStore.add("You did it!", "#4BB543");
-      finished.value = true;
+      state.finished = true;
       return;
     }
     toastStore.add("Get ready in 3, 2, 1...", "#4BB543");
-    loading.value = true;
+    state.loading = true;
     setTimeout(() => {
       reset();
       generateNumber();
-      loading.value = false;
+      state.loading = false;
     }, 3000);
   } else {
     toastStore.add("Nope!", "#f23333");
